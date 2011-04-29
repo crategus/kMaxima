388a409db86cad1a168e87d464eaef638daf8e9e
crategus <crategus@users.sourceforge.net>
2011-04-29 13:40:35 +0200

    Adding copyright notices

--- kmaxima/src/msystem.lisp
+++ kmaxima/src/msystem.lisp
@@ -1,4 +1,27 @@
-;;;; 2010-12-30 16:21:25
+;;; ----------------------------------------------------------------------------
+;;; msystem.lisp
+;;;
+;;; Copyright (C) 2011 Dr. Dieter Kaiser
+;;;
+;;; This file contains modified code from:
+;;;
+;;; Copyright (C) 1984, 1987 William Schelter, University of Texas
+;;; Copyright (C) 1981, 1982 Massachusetts Institute of Technology
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;;; ----------------------------------------------------------------------------
 
 (in-package :kmaxima)
 
