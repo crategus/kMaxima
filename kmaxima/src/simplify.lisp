bc54a8691655de473c1c39ff8181eb074e617066
crategus <crategus@users.sourceforge.net>
2011-07-23 21:46:49 +0200

    Adding the functions:
    add, add2, addn, mul, mul2, muln, inv, neg, sub, div, and power

--- kmaxima/src/simplify.lisp
+++ kmaxima/src/simplify.lisp
@@ -25,6 +25,84 @@
 
 (in-package :kmaxima)
 
+(defmvar $negdistrib t)
+
+;;; ----------------------------------------------------------------------------
+
+(defun add (&rest terms)
+  (if (and (cdr terms) (null (cddr terms)))
+      (apply #'add2 terms)
+      (apply #'addn `(,terms t))))
+
+(define-compiler-macro add (&rest terms)
+  (if (and (cdr terms) (null (cddr terms)))
+      `(add2 ,@terms))
+      `(addn (list ,@terms) t))
+
+(defun add2 (x y)
+  (cond ((and (numberp x) (numberp y)) (+ x y))
+        ((eql 0 x) y)
+        ((eql 0 y) x)
+        (t (simplifya `((mplus) ,x ,y) t))))
+
+(defun addn (terms simp-flag)
+  (cond ((null terms) 0)
+        ((null (cdr terms)) (simplifya (car terms) simp-flag))
+        (t (simplifya `((mplus) . ,terms) simp-flag))))
+
+;;; ----------------------------------------------------------------------------
+
+(defun mul (&rest factors)
+  (if (= (length factors) 2)
+      (apply #'mul2 factors)
+      (apply #'muln `(,factors t))))
+
+(define-compiler-macro mul (&rest factors)
+  (if (= (length factors) 2)
+      `(mul2 ,@factors))
+      `(muln (list ,@factors) t))
+
+(defun mul2 (x y)
+  (cond ((and (numberp x) (numberp y)) (* x y))
+        ((eql 1 x) y)
+        ((eql 1 y) x)
+        (t (simplifya `((mtimes) ,x ,y) t))))
+
+(defun muln (factors simp-flag)
+  (cond ((null factors) 1)
+        ((null (cdr factors)) (simplifya (car factors) simp-flag))
+        (t (simplifya `((mtimes) . ,factors) simp-flag))))
+
+;;; ----------------------------------------------------------------------------
+
+(defun inv (x)
+  (power x -1))
+
+(defun neg (x)
+  (declare (special $negdistrib))
+  (cond ((numberp x) (- x))
+        (t (let (($negdistrib t))
+             (simplifya `((mtimes) -1 ,x) t)))))
+
+(defun sub (x y)
+  (cond ((and (numberp x) (numberp y)) (- x y))
+        ((eql 0 y) x)
+        ((eql 0 x) (neg y))
+        (t (add x (neg y)))))
+
+(defun div (x y)
+  (if (eql 1 x)
+      (inv y)
+      (mul x (inv y))))
+
+(defun power (bas pow)
+  (cond ((eql 1 pow) bas)
+        (t (simplifya `((mexpt) ,bas ,pow) t))))
+
+;;; ----------------------------------------------------------------------------
+
 (defun simplifya (x y)
   (declare (ignore y))
   x)
+
+;;; ----------------------------------------------------------------------------
