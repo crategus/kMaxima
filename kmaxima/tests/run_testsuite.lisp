54e5077881bcfaf922a5ebb7596b198d35a0f260
crategus <crategus@users.sourceforge.net>
2011-07-23 21:39:01 +0200

    Initial commit of file to load and execute all tests.

---
+++ kmaxima/tests/run_testsuite.lisp
@@ -0,0 +1,15 @@
+;;;; Created on 2011-07-23 14:03:16
+
+(load "lisp/lisp-unit.lisp")
+
+(defpackage :kmaxima-tests
+  (:use :common-lisp :lisp-unit :kmaxima))
+
+(in-package :kmaxima-tests)
+
+(load "tests/rtest_mutils.lisp")
+(load "tests/rtest_parser.lisp")
+(load "tests/rtest_nformat.lisp")
+(load "tests/rtest_display.lisp")
+
+(run-all-tests :kmaxima-tests)
