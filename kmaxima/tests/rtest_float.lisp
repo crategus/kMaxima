7733ae35f77818a9f703cc22a53aa32088bce5d8
crategus <crategus@users.sourceforge.net>
2011-11-14 00:03:08 +0100

    Adding tests for intofp

--- kmaxima/tests/rtest_float.lisp
+++ kmaxima/tests/rtest_float.lisp
@@ -15,3 +15,14 @@
   (assert-equal '(#\5 #\. #\7 #\7 #\2 #\1 #\5 #\6 #\6 #\4 #\9 #\0 #\1 #\5 #\3
                           #\2 #\9 #\b #\- #\1 )
                 (fpformat ($bfloat '$%gamma))))
+
+(define-test intofp
+  (assert-equal '(0 0) (intofp 0))
+  (assert-equal '(36028797018963968 1) (intofp 1))
+  (assert-equal '(54043195528445952 1) (intofp 1.5))
+  (assert-equal '(56593902016227522 2) (intofp '$%pi))
+  (assert-equal '(48968212118944587 2) (intofp '$%e))
+  (assert-equal '(41592772053807304 0) (intofp '$%gamma))
+  (assert-equal '(58295818150454590 1) (intofp '$%phi)))
+                
+                
