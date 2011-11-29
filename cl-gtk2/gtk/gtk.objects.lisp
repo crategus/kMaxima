b8df3749dc5b1eb8c4abe957e42b9a70600345ee
crategus <crategus@users.sourceforge.net>
2011-11-29 01:10:26 +0100

    Moving functions to other files

--- cl-gtk2/gtk/gtk.objects.lisp
+++ cl-gtk2/gtk/gtk.objects.lisp
@@ -40,15 +40,6 @@
 (define-g-boxed-opaque tree-path "GtkTreePath"
   :alloc (gtk-tree-path-new))
 
-(defcfun (%gtk-tree-path-get-depth "gtk_tree_path_get_depth") :int
-  (path tree-path))
-
-(defcfun (%gtk-tree-path-get-indices "gtk_tree_path_get_indices") (:pointer :int)
-  (path tree-path))
-
-(defcfun (%gtk-tree-path-append-index "gtk_tree_path_append_index") :void
-  (path :pointer)
-  (index :int))
 
 (defun tree-path-indices (path)
   (tree-path-get-indices path))
@@ -76,43 +67,6 @@
 (export 'tree-path)
 (export 'tree-path-indices)
 
-(defcfun (tree-path-compare "gtk_tree_path_compare") :int
-  (tree-path-1 (g-boxed-foreign tree-path))
-  (tree-path-2 (g-boxed-foreign tree-path)))
-
-(export 'tree-path-compare)
-
-(defcfun (tree-path-next "gtk_tree_path_next") :void
-  (tree-path (g-boxed-foreign tree-path)))
-
-(export 'tree-path-next)
-
-(defcfun (tree-path-prev "gtk_tree_path_prev") :void
-  (tree-path (g-boxed-foreign tree-path)))
-
-(export 'tree-path-prev)
-
-(defcfun (tree-path-up "gtk_tree_path_up") :boolean
-  (tree-path (g-boxed-foreign tree-path)))
-
-(export 'tree-path-up)
-
-(defcfun (tree-path-down "gtk_tree_path_down") :void
-  (tree-path (g-boxed-foreign tree-path)))
-
-(export 'tree-path-down)
-
-(defcfun (tree-path-is-ancestor "gtk_tree_path_is_ancestor") :boolean
-  (tree-path (g-boxed-foreign tree-path))
-  (descendant (g-boxed-foreign tree-path)))
-
-(export 'tree-path-is-ancestor)
-
-(defcfun (tree-path-is-descendant "gtk_tree_path_is_descendant") :boolean
-  (tree-path (g-boxed-foreign tree-path))
-  (ancestor (g-boxed-foreign tree-path)))
-
-(export 'tree-path-is-descendant)
 
 (at-init () (gobject::type-initializer-call "gtk_tree_row_reference_get_type"))
 
@@ -157,78 +111,3 @@
   (height :int :initform 0))
 
 (export (boxed-related-symbols 'allocation))
-
-(defcfun (h-s-v-set-color "gtk_hsv_set_color") :void
-  (h-s-v (g-object h-s-v))
-  (h :double)
-  (s :double)
-  (v :double))
-
-(export 'h-s-v-set-color)
-
-(defcfun gtk-hsv-get-color :void
-  (h-s-v (g-object h-s-v))
-  (h (:pointer :double))
-  (s (:pointer :double))
-  (v (:pointer :double)))
-
-(defun h-s-v-get-color (h-s-v)
-  (with-foreign-objects ((h :double) (s :double) (v :double))
-    (gtk-hsv-get-color h-s-v h s v)
-    (values (mem-ref h :double) (mem-ref s :double) (mem-ref v :double))))
-
-(export 'h-s-v-get-color)
-
-(defcfun (h-s-v-set-metrics "gtk_hsv_set_metrics") :void
-  (h-s-v (g-object h-s-v))
-  (size :int)
-  (ring-width :int))
-
-(export 'h-s-v-set-metrics)
-
-(defcfun gtk-hsv-get-metrics :void
-  (h-s-v (g-object h-s-v))
-  (size (:pointer :int))
-  (ring-width (:pointer :int)))
-
-(defun h-s-v-get-metrics (h-s-v)
-  (with-foreign-objects ((size :int) (ring-width :int))
-    (gtk-hsv-get-metrics h-s-v size ring-width)
-    (values (mem-ref size :int) (mem-ref ring-width :int))))
-
-(export 'h-s-v-get-metrics)
-
-(defcfun (h-s-v-is-adjusting "gtk_hsv_is_adjusting") :boolean
-  (h-s-v (g-object h-s-v)))
-
-(export 'h-s-v-is-adjusting)
-
-(defcfun gtk-hsv-to-rgb :void
-  (h :double)
-  (s :double)
-  (v :double)
-  (r (:pointer :double))
-  (g (:pointer :double))
-  (b (:pointer :double)))
-
-(defun h-s-v-to-r-g-b (h s v)
-  (with-foreign-objects ((r :double) (g :double) (b :double))
-    (gtk-hsv-to-rgb h s v r g b)
-    (values (mem-ref r :double) (mem-ref g :double) (mem-ref b :double))))
-
-(export 'h-s-v-to-r-g-b)
-
-(defcfun gtk-rgb-to-hsv :void
-  (r :double)
-  (g :double)
-  (b :double)
-  (h (:pointer :double))
-  (s (:pointer :double))
-  (v (:pointer :double)))
-
-(defun r-g-b-to-h-s-v (r g b)
-  (with-foreign-objects ((h :double) (s :double) (v :double))
-    (gtk-rgb-to-hsv r g b h s v)
-    (values (mem-ref h :double) (mem-ref s :double) (mem-ref v :double))))
-
-(export 'r-g-b-to-h-s-v)
